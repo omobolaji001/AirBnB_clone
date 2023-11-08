d6adcda870c24e9c200a5585f550135af68eebe5
Bakijazzman <obafemiolabayo@gmail.com>
2023-11-08 15:22:01 +0100

    basemodel class

--- models/base_model.py
+++ models/base_model.py
@@ -1,3 +1,17 @@
 #!/usr//bn/python3
-#Base_model class 
+#Base_model class
+"""import modules here """
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """basemodel to be inherited by other classes """
+
+    def __init__(self):
+        """to be initialised wiith"""
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated at = datetime.now()
+
 
