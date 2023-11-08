d4a8911cd8df7cd5a9d716841ac0b15542ef2611
Bakijazzman <obafemiolabayo@gmail.com>
2023-11-08 16:05:02 +0100

    uuid

--- models/base_model.py
+++ models/base_model.py
@@ -10,8 +10,10 @@ class BaseModel:
 
     def __init__(self):
         """to be initialised wiith"""
-        self.id = str(uuid4())
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated at = datetime.now()
+        self.updated_at = datetime.now()
 
-
+    def __str__(self):
+        """printing the class """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
