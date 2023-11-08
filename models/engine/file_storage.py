3d0572cf2cf5eb18d874fe04b6ca10d53de701b4
omobolaji001 <omobolajiolayiwola11@gmail.com>
2023-11-08 19:37:55 +0100

    AirBnB clone

---
+++ models/engine/file_storage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# FileStorage class
+"""import modules here"""
+import json
+
+
+class FileStorage:
+    """ serializes instances to JSON file and 
+    deserializes JSON file to instances
+    """
